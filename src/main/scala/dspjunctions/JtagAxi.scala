610767ffff9e1d9f4dc3bb5b1515eaf4209b346f
Paul Rigge <rigge@berkeley.edu>
2017-03-30 11:18:12 -0700

    Add queues, incorporate feedback.

--- src/main/scala/dspjunctions/JtagAxi.scala
+++ src/main/scala/dspjunctions/JtagAxi.scala
@@ -4,9 +4,11 @@ package dspjunctions
 
 import cde._
 import chisel3._
+import chisel3.experimental.withClockAndReset
 import chisel3.util._
 import jtag._
 import junctions._
+import uncore.converters._
 
 case object DspChainIncludeJtag extends Field[Bool]
 
@@ -45,8 +47,12 @@ trait HasDspJtagModule extends HasDspJtagParameters {
     None
   }
 
-  def jtagCtrlAxiMasters: Seq[NastiIO] = jtagMaster.map(m => Seq(m.io.ctrlAxi)).getOrElse(Seq())
-  def jtagDataAxiMasters: Seq[NastiIO] = jtagMaster.map(m => Seq(m.io.ctrlAxi)).getOrElse(Seq())
+  def jtagCtrlAxiMasters: Option[NastiIO] = jtagMaster.map(m => {
+    m.io.ctrlAxi
+  })
+  def jtagDataAxiMasters: Option[NastiIO] = jtagMaster.map(m => {
+    m.io.ctrlAxi
+  })
 
   def jtagConnect: Unit = (io.jtag, jtagMaster) match {
     case (Some(jtag), Some(master)) =>
@@ -62,65 +68,60 @@ class JtagAxiMaster()(implicit p: Parameters) extends NastiModule()(p) {
     val ctrlAxi = new NastiIO
     val dataAxi = new NastiIO
   })
-
-  val tap = Module(new JtagTwoAxiMasterClocked(io.jtag.TCK.asClock, false.B)) // io.jtag.TRST))
-  io.jtag <> tap.io.jtag
-  tap.io.control.fsmAsyncReset := false.B
-
-  io.ctrlAxi <> tap.io.ctrlAxi
-  io.dataAxi <> tap.io.dataAxi
-}
-
-class JtagTwoAxiMasterClocked(modClock: Clock, modReset: Bool)(implicit val p: Parameters)
-  extends Module(override_clock=Some(modClock), override_reset=Some(modReset))
-  with HasNastiParameters {
-  
   val irLength = 4
+  val idcode = (BigInt(14), JtagIdcode(1, 1, 0x42))
 
-  val io = IO(new JtagBlockIO(irLength) {
-    val ctrlAxi = new NastiIO
-    val dataAxi = new NastiIO
-  })
+  val jtagClock = io.jtag.TCK.asClock
+  val jtagReset = Wire(Bool()) // reset=io.jtag.TRST
 
-  val ctrl_aw_chain = Module(new DecoupledSourceChain(Output(new NastiWriteAddressChannel)))
-  val ctrl_w_chain  = Module(new DecoupledSourceChain(Output(new NastiWriteDataChannel)))
-  val ctrl_b_chain  = Module(new DecoupledSinkChain(Output(new NastiWriteResponseChannel)))
-  val ctrl_ar_chain = Module(new DecoupledSourceChain(Output(new NastiReadAddressChannel)))
-  val ctrl_r_chain  = Module(new DecoupledSinkChain(Output(new NastiReadDataChannel)))
+  val (innerCtrlAxi, innerDataAxi) = withClockAndReset(clock = jtagClock, reset=jtagReset) {
+    val innerCtrlAxi = Wire(new NastiIO)
+    val innerDataAxi = Wire(new NastiIO)
+    val ctrl_aw_chain = Module(new DecoupledSourceChain(new NastiWriteAddressChannel))
+    val ctrl_w_chain  = Module(new DecoupledSourceChain(new NastiWriteDataChannel))
+    val ctrl_b_chain  = Module(new DecoupledSinkChain(new NastiWriteResponseChannel))
+    val ctrl_ar_chain = Module(new DecoupledSourceChain(new NastiReadAddressChannel))
+    val ctrl_r_chain  = Module(new DecoupledSinkChain(new NastiReadDataChannel))
 
-  val data_aw_chain = Module(new DecoupledSourceChain(Output(new NastiWriteAddressChannel)))
-  val data_w_chain  = Module(new DecoupledSourceChain(Output(new NastiWriteDataChannel)))
-  val data_b_chain  = Module(new DecoupledSinkChain(Output(new NastiWriteResponseChannel)))
-  val data_ar_chain = Module(new DecoupledSourceChain(Output(new NastiReadAddressChannel)))
-  val data_r_chain  = Module(new DecoupledSinkChain(Output(new NastiReadDataChannel)))
+    val data_aw_chain = Module(new DecoupledSourceChain(new NastiWriteAddressChannel))
+    val data_w_chain  = Module(new DecoupledSourceChain(new NastiWriteDataChannel))
+    val data_b_chain  = Module(new DecoupledSinkChain(new NastiWriteResponseChannel))
+    val data_ar_chain = Module(new DecoupledSourceChain(new NastiReadAddressChannel))
+    val data_r_chain  = Module(new DecoupledSinkChain(new NastiReadDataChannel))
 
-  val tapIO = JtagTapGenerator(irLength, Map(
-    0 -> ctrl_aw_chain,
-    1 -> ctrl_w_chain,
-    2 -> ctrl_b_chain,
-    3 -> ctrl_ar_chain,
-    4 -> ctrl_r_chain,
-    5 -> data_aw_chain,
-    6 -> data_w_chain,
-    7 -> data_b_chain,
-    8 -> data_ar_chain,
-    9 -> data_r_chain
-  ), idcode = None)
+    val tapIO = JtagTapGenerator(irLength, Map(
+      0 -> ctrl_aw_chain,
+      1 -> ctrl_w_chain,
+      2 -> ctrl_b_chain,
+      3 -> ctrl_ar_chain,
+      4 -> ctrl_r_chain,
+      5 -> data_aw_chain,
+      6 -> data_w_chain,
+      7 -> data_b_chain,
+      8 -> data_ar_chain,
+      9 -> data_r_chain
+    ), idcode = Some(idcode))
 
-  io.jtag <> tapIO.jtag
-  io.output <> tapIO.output
+    io.jtag <> tapIO.jtag
+    tapIO.control.fsmAsyncReset := false.B
+    jtagReset := tapIO.output.reset
+    // tapIO.output?
 
+    innerCtrlAxi.aw <> ctrl_aw_chain.io.interface
+    innerCtrlAxi.w  <> ctrl_w_chain.io.interface
+    innerCtrlAxi.b  <> ctrl_b_chain.io.interface
+    innerCtrlAxi.ar <> ctrl_ar_chain.io.interface
+    innerCtrlAxi.r  <> ctrl_r_chain.io.interface
 
-  io.ctrlAxi.aw <> ctrl_aw_chain.io.interface
-  io.ctrlAxi.w  <> ctrl_w_chain.io.interface
-  io.ctrlAxi.b  <> ctrl_b_chain.io.interface
-  io.ctrlAxi.ar <> ctrl_ar_chain.io.interface
-  io.ctrlAxi.r  <> ctrl_r_chain.io.interface
+    innerDataAxi.aw <> data_aw_chain.io.interface
+    innerDataAxi.w  <> data_w_chain.io.interface
+    innerDataAxi.b  <> data_b_chain.io.interface
+    innerDataAxi.ar <> data_ar_chain.io.interface
+    innerDataAxi.r  <> data_r_chain.io.interface
+    (innerCtrlAxi, innerDataAxi)
+  }
+  // io.jtag <> tap.io.jtag
 
-  io.dataAxi.aw <> data_aw_chain.io.interface
-  io.dataAxi.w  <> data_w_chain.io.interface
-  io.dataAxi.b  <> data_b_chain.io.interface
-  io.dataAxi.ar <> data_ar_chain.io.interface
-  io.dataAxi.r  <> data_r_chain.io.interface
+  io.ctrlAxi <> AsyncNastiFrom(from_clock=jtagClock, from_reset=jtagReset, from_source=innerCtrlAxi)
+  io.dataAxi <> AsyncNastiFrom(from_clock=jtagClock, from_reset=jtagReset, from_source=innerDataAxi)
 }
-
